b9de11fef5132cc8c9234ef4adbd2644df80fd53
Alexey Kupershtokh <alexey.kupershtokh@gmail.com>
2012-12-12 11:18:56 +0700

    updated arrays benchmarks

--- bench/arrays.js
+++ bench/arrays.js
@@ -1,61 +1,61 @@
-var Benchmark = require('benchmark');
 var assert = require('assert');
 try { lodash = require('lodash'); } catch (e) {};
 try { _ = require('underscore'); } catch (e) {};
 
-// array of 5 numeric elements
-arr1 = [1, 2, 3, 4, 5];
-
-// array of 1000 numeric elements
-arr2 = [];
-for (var i = 0; i < 1000; i++) {
-  arr2.push(i);
-}
+shared = require('./shared.js');
+arr1 = shared.arr1;
+arr2 = shared.arr2;
+arr3 = shared.arr3;
+arr4 = shared.arr4;
+arr5 = shared.arr5;
+arr6 = shared.arr6;
 
 // node-v8-clone js
 var Cloner = require('..').Cloner;
 cloner = new Cloner(false);
 
-// array 'for (var i = 0; i < l; i++)' cloner
-arr_for = function(arr) { var result = []; for (var i = 0, l = arr.length; i < l; i++) result.push(arr[i]); return result; }
-assert.deepEqual(arr1, arr_for(arr1));
+// slice
+slice = function(a) { return a.slice(); };
 
-// array 'for (var i = 0; i < l; i++)' cloner 2
-arr_for2 = function(arr) { var l = arr.length, result = new Array(l); for (var i = 0; i < l; i++) result[i] = arr[i]; return result; }
+// array 'for (var i = 0; i < l; i++)' cloner
+arr_for1 = function(a) { var b = []; for (var i = 0, l = a.length; i < l; i++) b.push(a[i]); return b; }
+assert.deepEqual(arr1, arr_for1(arr1));
+
+// array 'for (var i = 0; i < l; i++)' cloner
+arr_for2 = function(a) { var b = []; for (var i = 0, l = a.length; i < l; i++) b[i] = a[i]; return b; }
 assert.deepEqual(arr1, arr_for2(arr1));
 
+// array 'for (var i = 0; i < l; i++)' cloner 2
+arr_for3 = function(a) { var l = a.length, b = new Array(l); for (var i = 0; i < l; i++) b[i] = a[i]; return b; }
+assert.deepEqual(arr1, arr_for3(arr1));
+
+// array 'for (var i = 0; i < l; i++)' cloner 3
+arr_for4 = function(a) { var l = a.length, b = new Array(); for (var i = 0; i < l; i++) b[i] = a[i]; return b; }
+assert.deepEqual(arr1, arr_for4(arr1));
+
 // array 'for (i in arr)' cloner
-arr_for_in = function(arr) { var result = []; for(var i in arr) result.push(arr[i]); return result; }
+arr_for_in = function(a) { var b = []; for(var i in a) b.push(a[i]); return b; }
 assert.deepEqual(arr1, arr_for_in(arr1));
 
 // array 'for (i in arr) if (hasOwnProperty)' cloner
-arr_for_in_has = function(arr) { var result = []; for(var i in arr) if (arr.hasOwnProperty(i)) result.push(arr[i]); return result; }
+arr_for_in_has = function(a) { var b = []; for(var i in a) if (a.hasOwnProperty(i)) b.push(a[i]); return b; }
 assert.deepEqual(arr1, arr_for_in_has(arr1));
 
-var suite = new Benchmark.Suite;
-suite.on('cycle', function(event) {
-  console.log(String(event.target));
+// hybrid
+hybrid = function(a) { return (a.length > 100) ? a.slice() : arr_for3(a); };
+
+['arr1', 'arr2', 'arr3', 'arr4', 'arr5', 'arr6'].forEach(function(obj) {
+  shared.benchmark(obj, [
+    ['slice()',       'slice(' + obj + ')'],
+    ['[] ++ push',    'arr_for1(' + obj + ')'],
+    ['[] ++ [i]',     'arr_for2(' + obj + ')'],
+    ['Array(l) ++',   'arr_for3(' + obj + ')'],
+    ['Array() ++',    'arr_for4(' + obj + ')'],
+    ['hybrid',        'hybrid(' + obj + ')'],
+    ['for in',        'arr_for_in(' + obj + ')'],
+    ['for in has',    'arr_for_in_has(' + obj + ')'],
+    ['lodash.clone',  'lodash.clone(' + obj + ', false)'],
+    ['_.clone',       '_.clone(' + obj + ')'],
+    ['node-v8-clone', 'cloner.clone(' + obj + ')']
+  ]);
 });
-suite.on('complete', function() {
-  console.log('Fastest is ' + this.filter('fastest').pluck('name'));
-});
-
-suite.add('arr1 slice()                ', 'arr1.slice()');
-suite.add('arr1 []       for i < l i++ ', 'arr_for(arr1)');
-suite.add('arr1 Array(l) for i < l i++ ', 'arr_for2(arr1)');
-suite.add('arr1 for in                 ', 'arr_for_in(arr1)');
-suite.add('arr1 for in hasOwnProperty  ', 'arr_for_in_has(arr1)');
-suite.add('arr1 lodash.clone           ', 'lodash.clone(arr1, false)');
-suite.add('arr1 _.clone                ', '_.clone(arr1)');
-suite.add('arr1 node-v8-clone cloner   ', 'cloner.clone(arr1)');
-
-suite.add('arr2 slice()                ', 'arr2.slice()');
-suite.add('arr2 []       for i < l i++ ', 'arr_for(arr2)');
-suite.add('arr2 Array(l) for i < l i++ ', 'arr_for2(arr2)');
-suite.add('arr2 for in                 ', 'arr_for_in(arr2)');
-suite.add('arr2 for in hasOwnProperty  ', 'arr_for_in_has(arr2)');
-suite.add('arr2 lodash.clone           ', 'lodash.clone(arr2, false)');
-suite.add('arr2 _.clone                ', '_.clone(arr2)');
-suite.add('arr2 node-v8-clone cloner   ', 'cloner.clone(arr2)');
-
-suite.run({ 'async': true });
---
+++ bench/shared.js
@@ -0,0 +1,155 @@
+var plot = require('benchmark.js-plot').plot;
+var Benchmark = require('benchmark');
+
+var range = function(n) {
+  var result = [];
+  for (var i = 0; i < n; i++) {
+   result.push(i);
+  }
+  return result;
+};
+
+var date = module.exports.date = new Date();
+
+// obj1: 5 sting keys and values
+var obj1 = module.exports.obj1 = {'a': 'a', 'b': 'b', 'c': 'c', 'd': 'd', 'e': 'e'};
+
+// obj2: 5 integer keys and values
+var obj2 = module.exports.obj2 = {1: 1, 2: 2, 3: 3, 4: 4, 5: 5};
+
+// obj3: 100 string keys and values
+var obj3 = module.exports.obj3 = {};
+for (var i = 0; i < 100; i++) {
+  obj3['_' + i] = '_' + i;
+}
+
+// obj4: 100 integer keys and values
+var obj4 = module.exports.obj4 = {};
+for (var i = 0; i < 100; i++) {
+  obj4[i] = i;
+}
+
+// obj5: 10000 string keys and values
+var obj5 = module.exports.obj5 = {};
+for (var i = 0; i < 10000; i++) {
+  obj5['_' + i] = '_' + i;
+}
+
+// obj6: 10000 integer keys and values
+var obj6 = module.exports.obj6 = {};
+for (var i = 0; i < 10000; i++) {
+  obj6[i] = i;
+}
+
+// deepobj1: 5 sting keys and values
+var deepobj1 = module.exports.deepobj1 = {a: 'a', b: {c: 'c', d: 'd', e: 'e'}, f: 'f'};
+
+// deepobj2: 5 integer keys and values
+var deepobj2 = module.exports.deepobj2 = {1: 1, 2: {3: 3, 4: 4, 5: 5}, 6: 6};
+
+// deepobj3: 100 x 1 x 3 x 3 objects with string keys and values
+var deepobj3 = module.exports.deepobj3 = {};
+for (var i = 0; i < 100; i++) {
+  deepobj3['_' + i] = {
+    a: {
+      b: {c: 'c', d: 'd', e: 'e'},
+      f: {g: 'g', h: 'h', i: 'i'},
+      j: {k: 'k', l: 'l', m: 'm'}
+    }
+  };
+}
+
+// deepobj4: 100 x 1 x 3 x 3 objects with int keys and values
+var deepobj4 = module.exports.deepobj4 = {};
+for (var i = 0; i < 100; i++) {
+  deepobj4[i] = {
+    1: {
+      2: {3: 3, 4: 4, 5: 5},
+      6: {7: 7, 8: 8, 9: 9},
+      10: {11: 11, 12: 12, 13: 13}
+    }
+  };
+}
+
+// array of 1 numeric elements
+var arr1 = module.exports.arr1 = range(1);
+
+// array of 10 numeric elements
+var arr2 = module.exports.arr2 = range(10);
+
+// array of 100 numeric elements
+var arr3 = module.exports.arr3 = range(100);
+
+// array of 1000 numeric elements
+var arr4 = module.exports.arr4 = range(1000);
+
+// array of 10000 numeric elements
+var arr5 = module.exports.arr5 = range(10000);
+
+// array of 100000 numeric elements
+//var arr6 = module.exports.arr6 = range(100000);
+
+var arr6 = module.exports.arr6 = [];
+for (var i = 0; i < 100000; i++) {
+  switch(i % 6) {
+    case 0:
+      arr6.push(i);
+      break;
+    case 1:
+      arr6.push(i + 0.5);
+      break;
+    case 2:
+      arr6.push(Math.random() > 0.5);
+      break;
+    case 3:
+      arr6.push(new Date());
+      break;
+    case 4:
+      arr6.push(new RegExp('' + i));
+      break;
+    case 5:
+      arr6.push(new Function(' return ' + i));
+      break;
+  }
+}
+
+// deeparr1: 5 string keys and values
+var deeparr1 = module.exports.deeparr1 = ['a', ['b', 'c', 'd'], 'e'];
+
+// deeparr2: 100 x 1 x 3 x 3 nested elements with string values
+var deeparr2 = module.exports.deeparr2 = [];
+for (var i = 0; i < 100; i++) {
+  deeparr2[i] = [
+    [
+      ['c', 'd', 'e'],
+      ['g', 'h', 'i'],
+      ['k', 'l', 'm']
+    ]
+  ];
+}
+
+
+var f = module.exports.f = function (code) {
+  return new Function('next', 'count', 'while (count--) {' + code + '}; next();');
+};
+
+var benchmark = module.exports.benchmark = function(suite_name, benchmarks) {
+  var suite = new Benchmark.Suite;
+  suite.on('cycle', function(event) {
+    console.log(String(event.target));
+  });
+  suite.on('complete', function() {
+    plot(this, { path: 'results/' + suite_name + '.png' });
+    console.log('Fastest is ' + this.filter('fastest').pluck('name'));
+  });
+
+  var options = { maxTime: 0.1 };
+
+  benchmarks.forEach(function(tuple) {
+    var benchmark_name = tuple[0];
+    var code = tuple[1];
+    suite.add(benchmark_name, code, options);
+  });
+
+  suite.run();
+};
