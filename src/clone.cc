0677f95e49423aa2e4d2fbcc1d8ae7bf4cb1b8a7
Alexey Kupershtokh <alexey.kupershtokh@gmail.com>
2014-04-26 08:34:52 +0700

    Removed unnecessary code

--- src/clone.cc
+++ src/clone.cc
@@ -16,71 +16,9 @@ NAN_METHOD(Clone) {
   NanReturnValue(arg);
 }
 
-Handle<Value> _DeepClone(Handle<Value> value, Local<Object> stackMapA, Local<Object> stackMapB) {
-  if (value->IsObject()) {
-    NanScope();
-    Handle<Object> obj = Handle<Object>::Cast(value);
-
-    // handle repeated items
-    unsigned int hash = obj->GetIdentityHash();
-    Local<Value> stackMapAValue = stackMapA->Get(hash);
-    Local<Array> stackMapAArr;
-    Local<Array> stackMapBArr;
-    if (stackMapAValue->IsArray()) {
-      stackMapAArr = stackMapAValue.As<Array>();
-      stackMapBArr = stackMapA->Get(hash).As<Array>();
-      for (unsigned int j = 0; j < stackMapAArr->Length(); j++) {
-        Local<Value> stack = stackMapAArr->Get(j);
-        if (stack->StrictEquals(value)) {
-          return stackMapB->Get(hash).As<Array>()->Get(j);
-        }
-      }
-    } else {
-      stackMapAArr = Array::New();
-      stackMapBArr = Array::New();
-      stackMapA->Set(hash, stackMapAArr);
-      stackMapB->Set(hash, stackMapBArr);
-    }
-    stackMapAArr->Set(stackMapAArr->Length(), value);
-
-    // clone arrays
-    if (value->IsArray()) {
-      Handle<Array> cloned = Handle<Array>::Cast(obj->Clone());
-      stackMapBArr->Set(stackMapBArr->Length(), cloned);
-      for (unsigned int i = 0; i < cloned->Length(); i++) {
-        Local<Value> v = cloned->Get(i);
-        if (v->IsObject()) {
-          cloned->Set(i, _DeepClone(v, stackMapA, stackMapB));
-        }
-      }
-      return scope.Close(cloned);
-    }
-
-    // clone other objects
-    Handle<Object> cloned = obj->Clone();
-    stackMapBArr->Set(stackMapBArr->Length(), cloned);
-    Local<Array> props = cloned->GetOwnPropertyNames();
-    for(unsigned int i = 0; i < props->Length(); i++) {
-      Local<Value> key(props->Get(Integer::New(i)));
-      Local<Value> v = cloned->Get(key);
-      if (v->IsObject()) {
-        cloned->Set(key, _DeepClone(v, stackMapA, stackMapB));
-      }
-    }
-    return scope.Close(cloned);
-  }
-  return value;
-}
-
-NAN_METHOD(DeepClone) {
-  NanReturnValue(_DeepClone(args[0], Object::New(), Object::New()));
-}
-
 void Init(Handle<Object> target) {
   target->Set(NanSymbol("clone"),
       FunctionTemplate::New(Clone)->GetFunction());
-  target->Set(NanSymbol("deepclone"),
-      FunctionTemplate::New(DeepClone)->GetFunction());
 }
 
 NODE_MODULE(clone, Init)
